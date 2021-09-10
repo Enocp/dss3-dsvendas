9bdc5fb401784f877f3d109268283811fb4a42fc
Enocp <pierre.enoc@gmail.com>
2021-09-10 11:15:33 -0300

    :sparkles: group by search

--- backend/src/main/java/com/pierredev/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/pierredev/dsvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.pierredev.dsvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pierredev.dsvendas.dtos.SaleDTO;
+import com.pierredev.dsvendas.dtos.SaleSuccessDTO;
+import com.pierredev.dsvendas.dtos.SaleSumDTO;
 import com.pierredev.dsvendas.service.SaleService;
 
 @RestController
@@ -24,5 +28,17 @@ public class SaleController {
 		return ResponseEntity.ok().body(list);
 		
 	}
-
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/sucscess-by-selle")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
 }
+
--- backend/src/main/java/com/pierredev/dsvendas/service/SaleService.java
+++ backend/src/main/java/com/pierredev/dsvendas/service/SaleService.java
@@ -1,5 +1,7 @@
 package com.pierredev.dsvendas.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -7,6 +9,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pierredev.dsvendas.dtos.SaleDTO;
+import com.pierredev.dsvendas.dtos.SaleSuccessDTO;
+import com.pierredev.dsvendas.dtos.SaleSumDTO;
 import com.pierredev.dsvendas.entities.Sale;
 import com.pierredev.dsvendas.repositories.SaleRepository;
 import com.pierredev.dsvendas.repositories.SellerRepositoty;
@@ -26,5 +30,16 @@ public class SaleService {
 		Page<Sale> result = repository.findAll(pageable);
 		return result.map(x -> new SaleDTO(x));
 	}
-
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller() {
+	 	return repository.amountGroupedBySeller();
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> successGroupedBySeller() {
+		return repository.successGroupedBySeller();
+	}
 }
+
+
