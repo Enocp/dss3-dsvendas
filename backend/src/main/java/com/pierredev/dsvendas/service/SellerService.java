716904003e70757160418960e08f54346df1f741
Enocp <pierre.enoc@gmail.com>
2021-09-09 22:50:54 -0300

    :sparkles: add pagination

--- backend/src/main/java/com/pierredev/dsvendas/service/SellerService.java
+++ backend/src/main/java/com/pierredev/dsvendas/service/SellerService.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pierredev.dsvendas.dtos.SellerDTO;
 import com.pierredev.dsvendas.entities.Seller;
@@ -16,6 +17,7 @@ public class SellerService {
 	@Autowired
 	private SellerRepositoty repositoty;
 	
+	@Transactional(readOnly = true)
 	public List<SellerDTO> findALl() {
 		List<Seller> result = repositoty.findAll();
  		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
